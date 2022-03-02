2903f5a5dc687130232baa8563aef5dd6497c675
papamitsos93 <papamitsos93@gmail.com>
2022-03-02 23:41:49 +0200

    add index for storage at filters

--- mongo-init.js
+++ mongo-init.js
@@ -1,4 +1,4 @@
 db = db.getSiblingDB('resources')
 db.users.ensureIndex({"email": 1}, {"unique": true});
 db.users.ensureIndex({"username": 1}, {"unique": true});
-db.filters.ensureIndex({"storage_id": 1}, {"unique": true});
+db.storage_filters.ensureIndex({"storage_id": 1}, {"unique": true});
